3f566e7cec6192eba01e7c29f9b6a243e612ca82
CouffignalCedric <cedric.couffignal.sncf@gmail.com>
2023-03-13 16:40:40 +0100

    Spring1

---
+++ src/main/java/com/wildcodeschool/ProjectDoctor/ProjectDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.ProjectDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/ProjectDoctor/ProjectDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.ProjectDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
